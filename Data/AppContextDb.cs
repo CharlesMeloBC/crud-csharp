66487c1ae5974ee49a04ec8b031f378957a6003e
Charles <charles.melo145@gmail.com>
2024-10-06 11:22:20 -0300

    update and add nuget pakege

---
+++ Data/AppContextDb.cs
@@ -0,0 +1,13 @@
+﻿using crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Data
+{
+    public class AppContextDb : DbContext
+    {
+        public DbSet<Todo> Todos {get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        =>optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+        
+    }
+}
